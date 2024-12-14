58990f41e2976fcd577496e47b574bfc0fdbcc42
Shiv Singh <shivsinghcse@gmail.com>
2024-12-14 06:10:47 +0530

    vite setup

---
+++ Lecture_02/myFirst-app/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import MyComponent from './MyComponent'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <MyComponent />
+      {/* <MyComponent /> */}
+      {/* <MyComponent /> */}
+      {/* <MyComponent /> */}
+    </>
+  )
+}
+
+export default App
