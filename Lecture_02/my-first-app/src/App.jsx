ab4aacfca177c3a51a43b9796a1b22cf49148493
Shiv Singh <shivsinghcse@gmail.com>
2025-05-25 12:12:53 +0530

    vite setup complete

---
+++ Lecture_02/my-first-app/src/App.jsx
@@ -0,0 +1,13 @@
+import MyComponent from './MyComponent'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <MyComponent />
+    </>
+  )
+}
+
+export default App
---
+++ Lecture_02/my-first-app/src/MyComponent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MyComponent = () => {
+    return (
+        <>
+        <h1>Hi, this is my first component!!</h1>
+        </>
+    )
+}
+
+export default MyComponent;
